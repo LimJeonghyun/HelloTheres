1a88ffe10b925d4495b64f93e5761557f334fd9e
LimJeonghyun <ljh01030913@gmail.com>
2024-07-09 16:42:59 +0900

    UIKit proj

---
+++ HelloThere_UIKit/HelloThere_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloThere_UIKit
+//
+//  Created by 임정현 on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
